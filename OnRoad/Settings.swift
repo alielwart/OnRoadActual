7b3740253134165d1e55f7873f1be9167aea7f27
aban4 <aban@umich.edu>
2022-10-09 18:11:21 -0400

    settings on one page

--- OnRoad/Settings.swift
+++ OnRoad/Settings.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct Settings: View {
+    @State private var speed = 50.0
+    @State private var isEditing = false
     var body: some View {
         NavigationView {
             
@@ -18,19 +20,47 @@ struct Settings: View {
                     Toggle(isOn: .constant(true), label: {
                         Text("Night Mode")
                     })
+                    
                 }
                 
                 //TODO: add sections
-                Section(header: Text("Assecibility")){
+                Section(header: Text("Accessibility")){
                     
-                    NavigationLink("Vibration Settings", destination: Vibration_Settings())
+//                    NavigationLink("Vibration Settings", destination: Vibration_Settings())
+                    
+                  
+                            Section{
+                                
+                                //TODO: implement working vibration on off
+                                Toggle(isOn: .constant(true), label: {
+                                    Text("Vibration")
+                                })
+                            }
+                    
+                            Text("Change Vibration Intensity")
+                            Slider(
+                                value: $speed,
+                                in: 0...100,
+                                step: 5,
+                                onEditingChanged: { editing in
+                                    isEditing = editing
+                                }
+                            )
+                            //TODO: change step to not have .000000000
+                            .padding(.horizontal, 20.0)
+                            Text("\(speed)")
+                                .foregroundColor(isEditing ? .red : .blue)
+                        }
+                    
+                    
+
                 }
                 .navigationTitle("Settings")
             }
         }
         
     }
-}
+
 
 struct Settings_Previews: PreviewProvider {
     static var previews: some View {
